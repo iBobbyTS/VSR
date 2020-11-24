62d115fc5dcc20ff413e4ac9b671506dbd1b8c68
iBobby <46138297+iBobbyTS@users.noreply.github.com>
2020-11-24 21:43:47 +0800

    Create build.py

---
+++ build.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+python_executable = sys.executable
+os.system('git clone https://github.com/xinntao/BasicSR.git')
+os.chdir('BasicSR')
+os.system(f'{python_executable} -m pip install -r requirements.txt')
+os.system(f'{python_executable} setup.py develop')
