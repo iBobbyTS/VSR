a966d5922e020e6185837fa340f0a97e98855e81
iBobby <46138297+iBobbyTS@users.noreply.github.com>
2020-11-27 18:58:04 +0800

    Update EDVR.py

--- SRers/EDVR.py
+++ SRers/EDVR.py
@@ -1,6 +1,7 @@
 import math
 
 import numpy
+import cv2
 import torch
 
 from basicsr.models.archs.edvr_arch import EDVR
@@ -19,22 +20,23 @@ def cal_split(h_w, max_side_length):
 
 
 class SRer:
-    models = {
-        'ld':   [EDVR(num_feat=128, num_reconstruct_block=40, hr_in=True, with_predeblur=True).cuda(), 5, 1],
-        'ldc':  [EDVR(num_feat=128, num_reconstruct_block=40, hr_in=True, with_predeblur=True).cuda(), 5, 1],
-        'l4r':  [EDVR(num_feat=128, num_reconstruct_block=40).cuda(), 5, 4],
-        'l4v':  [EDVR(num_feat=128, num_reconstruct_block=40, num_frame=7, center_frame_idx=3).cuda(), 7, 4],
-        'l4br': [EDVR(num_feat=128, num_reconstruct_block=40, with_predeblur=True).cuda(), 5, 4],
-        'm4r':  [EDVR(with_tsa=False).cuda(), 5, 4],
-        'mt4r': [EDVR().cuda(), 5, 4]
+    models = {  # 'model_name': [model, num_of_frame, enlarge_factor:ef, multiple, max_res]
+        'ld':   [EDVR(num_feat=128, num_reconstruct_block=40, hr_in=True, with_predeblur=True).cuda(), 5, 1, 16, 994],
+        'ldc':  [EDVR(num_feat=128, num_reconstruct_block=40, hr_in=True, with_predeblur=True).cuda(), 5, 1, 16, 994],
+        'l4r':  [EDVR(num_feat=128, num_reconstruct_block=40).cuda(), 5, 4, 4, 288],
+        'l4v':  [EDVR(num_feat=128, num_reconstruct_block=40, num_frame=7, center_frame_idx=3).cuda(), 7, 4, 4, 256],
+        'l4br': [EDVR(num_feat=128, num_reconstruct_block=40, with_predeblur=True).cuda(), 5, 4, 4, 256],
+        'm4r':  [EDVR(with_tsa=False).cuda(), 5, 4, 4, 416],
+        'mt4r': [EDVR().cuda(), 5, 4, 4, 416]
     }
 
     def __init__(self, model_name, model_path, height, width):
-        self.model, self.num_frame, self.enlarge = self.models[model_name]
+        self.model, self.num_frame, self.enlarge, ef, max_res = self.models[model_name]
         self.height, self.width = height, width
+        self.splition = cal_split((self.dim[0], self.dim[1]), 256)
 
-        self.h_w = [int(math.ceil(height / 32) * 32 - height) if height % 32 else 0,
-                    int(math.ceil(width / 32) * 32) - width if width % 32 else 0]
+        self.h_w = [int(math.ceil(height / ef) * ef - height) if height % ef else 0,
+                    int(math.ceil(width / ef) * ef) - width if width % ef else 0]
         self.dim = [height + self.h_w[0], width + self.h_w[1]]
         self.pader = torch.nn.ReplicationPad2d([0, self.h_w[1], 0, self.h_w[0]])
 
@@ -43,8 +45,6 @@ class SRer:
 
     def init_batch(self, video):
         self.batch = torch.cuda.FloatTensor(1, self.num_frame, 3, self.dim[0], self.dim[1])
-        print(self.batch.size())
-        self.splition = cal_split((self.dim[0], self.dim[1]), 256)
         frame = self.ndarray2tensor(video.read()[1])
         for i in range(self.num_frame // 2 + 1):
             self.batch[0, i] = frame
@@ -57,14 +57,16 @@ class SRer:
         return out_frame
 
     def sr(self, f):
+        e = self.enlarge
         if f[0]:
             self.batch[0, -1] = self.ndarray2tensor(f[1])
         outputs = []
         for i in self.splition:
             outputs.append((self.model(self.batch[:, :, :, i[0]:i[1], i[2]:i[3]])*255.0).clamp(0, 255).byte().squeeze().permute(1, 2, 0).cpu().numpy())
         self.batch[0, :-1] = self.batch.clone()[0, 1:]
-        output = numpy.zeros((self.height*self.enlarge, self.width*self.enlarge, 3), dtype=numpy.uint8)
+        output = numpy.zeros((self.height*e, self.width*e, 3), dtype=numpy.uint8)
         for slice_, tensor in zip(self.splition, outputs):
-            output[slice_[0]*self.enlarge:slice_[1]*self.enlarge, slice_[2]*self.enlarge:slice_[3]*self.enlarge, :] = tensor
-        output = output[:, :, [2, 1, 0]]
+            shape = output[slice_[0]*e:slice_[1]*e, slice_[2]*e:slice_[3]*e].shape
+            output[slice_[0]*e:slice_[1]*e, slice_[2]*e:slice_[3]*e] = tensor[:shape[0], :shape[1]]
+        output = output[self.h_w[0]*e:, self.h_w[1]*e:, [2, 1, 0]]
         return output
