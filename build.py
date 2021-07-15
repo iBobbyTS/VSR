c94f6817007d394a240521072aa6a2c5f2c04193
iBobby <46138297+iBobbyTS@users.noreply.github.com>
2021-07-15 21:51:49 +0800

    Update build.py

--- build.py
+++ build.py
@@ -5,4 +5,4 @@ python_executable = sys.executable
 os.system('git clone https://github.com/xinntao/BasicSR.git')
 os.chdir('BasicSR')
 os.system(f'{python_executable} -m pip install -r requirements.txt')
-os.system(f'{python_executable} setup.py develop')
+os.system(f'BASICSR_EXT=True {python_executable} setup.py develop')
